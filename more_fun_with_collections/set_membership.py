e958868d03ec71285880a7f1f18a19404fac964e
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-10-12 22:15:01 -0500

    Initial Commit

---
+++ more_fun_with_collections/set_membership.py
@@ -0,0 +1,12 @@
+"""
+Author: Tyler Hochstetler
+The purpose of this program is to practice using sets in Python.
+"""
+
+
+def in_set(a_set, element):
+  pass
+
+
+if __name__ == "__main__":
+  pass
---
+++ test/test_set_membership.py
@@ -0,0 +1,19 @@
+import unittest
+from more_fun_with_collections import set_membership
+
+
+class TestSetMembership(unittest.TestCase):
+  def test_in_set_true(self):
+    test_set = ('abcdef')
+    element = 'd'
+    self.assertTrue(set_membership.in_set(test_set, element))
+
+
+  def test_in_set_false(self):
+    false_test_set = ('12345')
+    false_element = 0
+    self.assertFalse(set_membership.in_set(false_test_set, false_element))
+
+
+if __name__ == "__main__":
+  unittest.main()
