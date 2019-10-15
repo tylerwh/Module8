f0a2e693882e78000d08de5cd3f5d58df133b56b
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-10-14 23:50:31 -0500

    Test passes

--- more_fun_with_collections/calculate_half_birthday.py
+++ more_fun_with_collections/calculate_half_birthday.py
@@ -3,10 +3,13 @@ Author: Tyler Hochstetler
 The purpose of this program is to calculate the half birthday based upon the most recent birthday. 
 """
 import datetime
+from datetime import timedelta
 
 
 def half_birthday(date):
-  pass
+  """Accepts a datetime of recent birthday and returns the date 6 months later."""
+  half_b_day = date + timedelta(days=183.5)
+  return half_b_day
 
 
 if __name__ == "__main__":
--- test/test_calculate_half_birthday.py
+++ test/test_calculate_half_birthday.py
@@ -1,13 +1,14 @@
 import unittest
 import datetime
+from datetime import timedelta
 from more_fun_with_collections import calculate_half_birthday as chf
 
 
 class TestHalfBirthday(unittest.TestCase):
   def test_half_birthday(self):
     b_day = datetime.date(2019, 2, 14)
-    result = datetime.date(2020, 8, 14)
-    self.assertAlmostEqual(chf.half_birthday(b_day), result)
+    result = b_day + timedelta(days=183.5)
+    self.assertEqual(chf.half_birthday(b_day), result)
 
 
 if __name__ == "__main__":
